7d2c1e41768aeff34934fbf90354cd17a92a70b1
Giovanni Bruno <giovannibruno128@gmail.com>
2022-10-17 16:32:16 -0300

    inital setup

---
+++ ThisORThatRedux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThisORThatRedux
+//
+//  Created by Giovanni Bruno on 17/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThisORThatRedux/ThisORThatReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThisORThatReduxApp.swift
+//  ThisORThatRedux
+//
+//  Created by Giovanni Bruno on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ThisORThatReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
